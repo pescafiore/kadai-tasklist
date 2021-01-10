d08501ce5930e6c8891dac77ee4b71fe19fd4f20
akihiro.noguchi <a-noguchi@foagram.co.jp>
2021-01-10 08:20:22 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  root to: "tasks#index"
-  get "signup", to: "users#new"
   
+  root to: "tasks#index"
+
+  get "signup", to: "users#new"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
   resources :tasks
   resources :users, only: [:new, :create]
   
